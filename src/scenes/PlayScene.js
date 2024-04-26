cf3afdc61ba01245bfaef602e973d5b745741566
MikaPS <mikaps7@gmail.com>
2024-04-26 11:45:34 -0700

    adding check hit

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -124,4 +124,16 @@ class PlayScene extends Phaser.Scene {
       console.log("you won!!!");
     }
   }
+  checkHit(player, enemy) {
+    if (
+      player.x < enemy.x + enemy.width &&
+      player.x + player.width > enemy.x &&
+      player.y < enemy.y + enemy.height &&
+      player.y + player.height > enemy.y
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
