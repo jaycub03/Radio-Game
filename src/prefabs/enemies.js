9aaa11b89ffe2a7669944d0fc2074536b5fc0db9
MikaPS <mikaps7@gmail.com>
2024-05-01 20:02:24 -0700

    working on synching lyrics

--- src/prefabs/enemies.js
+++ src/prefabs/enemies.js
@@ -5,35 +5,43 @@ class enemies extends Phaser.GameObjects.Sprite {
     this.moveSpeed = moveSpeed;
     this.initialX = x;
     this.initialY = y;
+    this.stop = false;
   }
 
   decreaseSpeed() {
     this.moveSpeed -= 0.05;
   }
-  update() {
-    const px = game.config.width / 2;
-    const py = game.config.height / 2;
-    const x = this.x;
-    const y = this.y;
 
-    const rotation = Phaser.Math.Angle.Between(x, y, px, py);
-    this.setRotation(rotation);
-    //this.scene.physics.velocityFromRotation(this.rotation, this.moveSpeed);
-    if (x >= px && y >= py) {
-      this.x -= this.moveSpeed;
-      this.y -= this.moveSpeed;
-    }
-    if (x >= px && y <= py) {
-      this.x -= this.moveSpeed;
-      this.y += this.moveSpeed;
-    }
-    if (x <= px && y >= py) {
-      this.x += this.moveSpeed;
-      this.y -= this.moveSpeed;
-    }
-    if (x <= px && y <= py) {
-      this.x += this.moveSpeed;
-      this.y += this.moveSpeed;
+  setStop(stop) {
+    this.stop = stop;
+  }
+
+  update() {
+    if (this.stop == false) {
+      const px = game.config.width / 2;
+      const py = game.config.height / 2;
+      const x = this.x;
+      const y = this.y;
+
+      const rotation = Phaser.Math.Angle.Between(x, y, px, py);
+      this.setRotation(rotation);
+      //this.scene.physics.velocityFromRotation(this.rotation, this.moveSpeed);
+      if (x >= px && y >= py) {
+        this.x -= this.moveSpeed;
+        this.y -= this.moveSpeed;
+      }
+      if (x >= px && y <= py) {
+        this.x -= this.moveSpeed;
+        this.y += this.moveSpeed;
+      }
+      if (x <= px && y >= py) {
+        this.x += this.moveSpeed;
+        this.y -= this.moveSpeed;
+      }
+      if (x <= px && y <= py) {
+        this.x += this.moveSpeed;
+        this.y += this.moveSpeed;
+      }
     }
   }
 
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -20,7 +20,7 @@ class PlayScene extends Phaser.Scene {
     this.load.image("player", "../assets/player.png");
 
     //best song ever
-    this.load.audio("music", "../assets/baby_cut.wav")
+    this.load.audio("music", "../assets/baby_cut.wav");
   }
 
   create() {
@@ -45,10 +45,46 @@ class PlayScene extends Phaser.Scene {
     this.song = this.sound.add("music");
     this.song.setVolume(0.3);
     this.song.play();
+    this.startTime = this.time.now;
+    this.breakTimes = [
+      2 * 1000,
+      2 * 1000,
+      2 * 1000,
+      2 * 1000,
+      6 * 1000,
+      6 * 1000,
+      6 * 1000,
+      6 * 1000,
+      10 * 1000,
+      10 * 1000,
+      10 * 1000,
+      10 * 1000,
+      14 * 1000,
+      14 * 1000,
+      14 * 1000,
+      14 * 1000,
+      55 * 1000,
+      55 * 1000,
+      55 * 1000,
+      55 * 1000,
+      69 * 1000,
+      69 * 1000,
+      69 * 1000,
+      69 * 1000,
+      94 * 1000,
+      94 * 1000,
+      94 * 1000,
+      94 * 1000,
+    ];
 
     // words
     this.songLyrics =
-      "This is a song more lyrics woooo nowgn nsodg gklsg sdklg more words and even more words yay yay yay".toLowerCase();
+      "delay delay delay delay Oh woah Oh woah delay delay delay delay Oh woah oh woah delay delay delay delay oh woah oh woah delay delay delay delay You know you love me Just shout whenever You are my love And we will never Are we an item? Were just friends Said Theres another and My first love broke my heart and I was like Baby baby baby oh Like baby baby baby no Like baby baby baby oh delay delay delay delay I thought youd always be mine mine Baby baby baby oh Like baby baby baby no Like baby baby baby oh delay delay delay delay I thought youd always be mine mine Oh for you I wouldve done whatever I wanna play it cool Ill buy you anything Im in pieces baby fix me till you wake me I just cant believe my first love wont be around Im like Baby baby baby oh Like baby baby baby no Like baby baby baby oh I thought youd always be mine mine Baby baby baby oh Like baby baby baby no Like baby baby baby oh I thought youd always be mine mine"
+        .toLowerCase()
+        .replace(",", "")
+        .replace("?", "")
+        .replace("'", "")
+        .replace(",", "");
     this.words = this.songLyrics.split(" ");
     this.enemyWords = ["", "", "", ""];
     // detect key presses
@@ -103,15 +139,16 @@ class PlayScene extends Phaser.Scene {
     ).setOrigin(0, 0);
     this.enemy3 = new enemies(
       this,
-      game.config.width * 0.9,
+      0,
       game.config.height / 2,
+
       "enemy",
       0,
       0.1
     ).setOrigin(0, 0);
     this.enemy4 = new enemies(
       this,
-      0,
+      game.config.width * 0.9,
       game.config.height / 2,
       "enemy",
       0,
@@ -122,7 +159,7 @@ class PlayScene extends Phaser.Scene {
     this.enemyWord1 = this.add.text(this.enemy1.x, this.enemy1.y, "");
     this.enemyWord2 = this.add.text(this.enemy2.x, this.enemy2.y, "");
     this.enemyWord3 = this.add.text(this.enemy3.x, this.enemy3.y, "");
-    this.enemyWord4 = this.add.text(this.enemy4.x, this.enemy4.y, "");
+    this.enemyWord4 = this.add.text(this.enemy4.x, this.enemy4.y - 20, "");
     this.enemyTextObjects = [
       this.enemyWord1,
       this.enemyWord2,
@@ -144,7 +181,7 @@ class PlayScene extends Phaser.Scene {
     this.beatDuration = (60000 / this.BPM) * 3;
 
     // Draw a rectangle
-    this.lengthOfPowerUp = 9; // # of words before it turns off
+    this.lengthOfPowerUp = 25; // # of words before it turns off
     this.startOfPowerUp = parseInt(this.words.length - this.words.length / 5);
     this.characterSize = 0;
     this.powerUp = 0;
@@ -195,7 +232,37 @@ class PlayScene extends Phaser.Scene {
           this.song.stop();
           this.scene.start("GameOverScene", { win: this.win });
         } else {
-          this.enemyWords[i] = this.words.shift();
+          let newWord = this.words.shift();
+
+          console.log("word:", newWord, this.breakTimes);
+          if (newWord == "delay") {
+            this.currentTime = this.time.now;
+            this.currentBreak = this.breakTimes.shift();
+
+            // console.log(
+            //   this.startTime,
+            //   this.currentTime,
+            //   this.startTime - this.currentTime,
+            //   this.breakTimes[0] - (this.currentTime - this.startTime)
+            // );
+            if (this.currentTime - this.startTime <= this.breakTimes[0]) {
+              this.enemyWords[i] = " ";
+              this.allEnemies[i].setStop(true);
+              this.time.delayedCall(
+                this.currentBreak - (this.currentTime - this.startTime),
+                function () {
+                  this.allEnemies[i].reset();
+                  this.newWords();
+                  this.enemyWords[i] = "";
+                  this.allEnemies[i].setStop(false);
+                },
+                [],
+                this
+              );
+            }
+          } else {
+            this.enemyWords[i] = newWord;
+          }
         }
       }
     }
@@ -216,9 +283,9 @@ class PlayScene extends Phaser.Scene {
     this.enemyWord2.x = this.enemy2.x;
     this.enemyWord2.y = this.enemy2.y;
     this.enemyWord3.x = this.enemy3.x;
-    this.enemyWord3.y = this.enemy3.y;
+    this.enemyWord3.y = this.enemy3.y - 20;
     this.enemyWord4.x = this.enemy4.x;
-    this.enemyWord4.y = this.enemy4.y - 20;
+    this.enemyWord4.y = this.enemy4.y;
     this.enemyWord1.text = this.enemyWords[0];
     this.enemyWord2.text = this.enemyWords[1];
     this.enemyWord3.text = this.enemyWords[2];
